6de64217cb7d623c0645d86e6a1d5e2794ed8e4e
ueg1990 <uehtesham90@gmail.com>
2014-10-17 00:11:39 -0400

    Install ParePy

---
+++ wearhacks_website/app.py
@@ -0,0 +1,15 @@
+'''
+This module runs the wearhacks web app
+
+'''
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
